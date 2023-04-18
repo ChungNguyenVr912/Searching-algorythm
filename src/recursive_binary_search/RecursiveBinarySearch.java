e781f90a323ec6c9c3a203ac1c0d46ce85bfd9a3
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-18 13:46:32 +0700

    Update RecursiveBinarySearch.java

--- src/recursive_binary_search/RecursiveBinarySearch.java
+++ src/recursive_binary_search/RecursiveBinarySearch.java
@@ -3,25 +3,21 @@ package recursive_binary_search;
 
 public class RecursiveBinarySearch {
     static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
-    static int ReBinarySearch(int key, int left, int right){
-        if (left == right){
-            if (list[left] == key){
-                return left;
-            }else {
-                return -1;
-            }               
+    static int ReBinarySearch(int[] list, int key, int left, int right){
+        if (left > right){
+            return -1;
         }
         int mid = (right - left)/2 + left;
         if (list[mid] < key){
-            return ReBinarySearch(key,mid+1,right);
+            return ReBinarySearch(list, key,mid+1,right);
         } else if (list[mid] > key) {
-            return ReBinarySearch(key, left, mid -1);
+            return ReBinarySearch(list, key, left, mid -1);
         }else {
             return mid;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(ReBinarySearch(77,0, list.length-1));
+        System.out.println(ReBinarySearch(list, 60,0, list.length-1));
     }
 }
